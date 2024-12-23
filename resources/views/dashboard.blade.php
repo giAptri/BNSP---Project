61fdf524ffe9d714b2d176f17554736c483c27c2
Argya Arya Putri <114375031+giAptri@users.noreply.github.com>
2024-12-23 10:59:01 +0700

    TechNova CRUD Project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+  
+@section('contents')
+  <div class="row">
+        <h3>Electronic Devices</h3>
+  </div>
+  <div>
+     <p>Electronic Devices Aplication</p>
+  </div>
+@endsection
